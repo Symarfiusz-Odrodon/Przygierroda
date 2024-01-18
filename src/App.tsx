0d937e05623e993377cda2f78ddc968b6689bef7
Symarfiusz-Renesans <patalitaszymon@gmail.com>
2024-01-18 19:25:35 +0100

    dodanie istniejącego projektu

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { useState, createContext } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Context } from 'vm';
+
+import logo from'./zdjęcia/logo.png'; 
+import OPrzygierrodzie from './podstrony/oPrzygierrodzie';
+
+export const AppContext: Context = createContext("");
+
 function App() {
+
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="przyklejonaCzęść"  className="sticky">
+        <header>
+          <div className="logo"><img className="zdjęcieLogo" width={200} height={200} src={logo} alt="logo" /></div>
+          <div className="przybocznyPasek">
+            <img src="" alt="" className="ustawieniaStrony" />
+            <img src="" alt="" className="ustawieniaStrony" />
+            <img src="" alt="" className="ustawieniaStrony" />
+          </div>
+        </header>
+        <nav>
+          <Link to="/podstrony/oPrzygierrodzie"><div className="przyciskNaglowka">O Nas</div></Link>
+          <Link to="/"><div className="przyciskNaglowka">Nasze Gry</div></Link>
+          <Link to="/"><div className="przyciskNaglowka">Kontakt</div></Link>
+          <Link to="/"><div className="przyciskNaglowka">Media Socjalne</div></Link>
+          <Link to="/"><div className="przyciskNaglowka">Wesprzyj Nas!</div></Link>
+          <Link to="/"><div className="przyciskNaglowka">Praca</div></Link>
+          <Link to="/"><div className="przyciskNaglowka">Regulamin</div></Link>
+        </nav>
+      </div>
+      <div id="stałaCzęść">
+        <section id="lewaNauka"></section>
+        <section id="głównaŚrodkowa">
+                <Routes>
+                  <Route path='/' element={<OPrzygierrodzie/>}/>
+                  <Route path='/'/>
+                  <Route/>
+                  <Route/>
+                </Routes>
+        </section>
+        <section id="prawaDrzewo"></section>
+      </div>
     </div>
+    </Router>
   );
 }
 
---
+++ src/podstrony/oPrzygierrodzie.tsx
@@ -0,0 +1,11 @@
+import "./podstrony.css";
+
+const OPrzygierrodzie = () => {
+    return (
+        <div className="zawartośćPodstrony">
+            To jest podstrona dla czterech pierwszych zakładek
+        </div>
+    )
+};
+
+export default OPrzygierrodzie;
