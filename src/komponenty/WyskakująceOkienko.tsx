95ab14d9f8384c2537fccdf0346e1f510132f50d
Symarfiusz-Renesans <patalitaszymon@gmail.com>
2024-02-01 13:34:57 +0100

    dodanie opcji zamknięcia okienka
    nie trzeba teraz używać esc wy z niego wychodzić

--- src/komponenty/WyskakująceOkienko.tsx
+++ src/komponenty/WyskakująceOkienko.tsx
@@ -1,20 +1,23 @@
 import "./okienko.css"
 
+import { useState } from "react";
 import Popup from "reactjs-popup";
 
 import ciasteczko from '../zdjęcia/ikony/cookies-icon.png';
 import krzyżyk from "../zdjęcia/ikony/krzyżyk.png"
 
 export const WyskakująceOkienko = () => {
-    
+    const [krzyżykOkienka, ustawKrzyżykOkienka] = useState(false);
     return (
-        <Popup trigger={<div className="ustawieniaStrony"><img src={ciasteczko} alt="ciasteczko"/></div>}>
+      <div className="ustawieniaStrony" onClick={() => ustawKrzyżykOkienka(true)}><img src={ciasteczko} alt="ciasteczko"/>
+        <Popup open={krzyżykOkienka}>
           <div id="główny">
             <div id="wnętrze">
-              <div id="krzyżyk"><img src={krzyżyk} alt="zamknij" /></div>
+              <div id="krzyżyk" onClick={()=>ustawKrzyżykOkienka(false)}><img src={krzyżyk} alt="zamknij" /></div>
               Twój stary
             </div>
           </div>
         </Popup>
+      </div>
     )
 }
