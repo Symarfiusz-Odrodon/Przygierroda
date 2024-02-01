fdcf13caebaa74207f0cb64708960e7069aaca04
Symarfiusz-Renesans <patalitaszymon@gmail.com>
2024-02-01 11:00:09 +0100

    dodanie podstawowego stylu do okienka.
    Teraz się to jakoś przynajmniej prezentuje.

--- src/komponenty/WyskakująceOkienko.tsx
+++ src/komponenty/WyskakująceOkienko.tsx
@@ -3,12 +3,18 @@ import "./okienko.css"
 import Popup from "reactjs-popup";
 
 import ciasteczko from '../zdjęcia/ikony/cookies-icon.png';
+import krzyżyk from "../zdjęcia/ikony/krzyżyk.png"
 
 export const WyskakująceOkienko = () => {
     
     return (
         <Popup trigger={<div className="ustawieniaStrony"><img src={ciasteczko} alt="ciasteczko"/></div>}>
-          <div id="główny">Twój stary</div>
+          <div id="główny">
+            <div id="wnętrze">
+              <div id="krzyżyk"><img src={krzyżyk} alt="zamknij" /></div>
+              Twój stary
+            </div>
+          </div>
         </Popup>
     )
 }
