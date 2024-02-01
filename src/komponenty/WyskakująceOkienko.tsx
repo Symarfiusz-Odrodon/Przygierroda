487137b6c11c6cffd8c507d678039a67c3e84607
Symarfiusz-Renesans <patalitaszymon@gmail.com>
2024-02-01 14:27:14 +0100

    uzupełnienie okienka.

--- src/komponenty/WyskakująceOkienko.tsx
+++ src/komponenty/WyskakująceOkienko.tsx
@@ -14,7 +14,26 @@ export const WyskakująceOkienko = () => {
           <div id="główny">
             <div id="wnętrze">
               <div id="krzyżyk" onClick={()=>ustawKrzyżykOkienka(false)}><img src={krzyżyk} alt="zamknij" /></div>
-              Twój stary
+              
+              <h1>Używamy ciasteczek!</h1>
+              <b><p>I choć wierzemy, że czasami trzeba osłodzić swoje życie, tak też rozumiemy Twoją chęć prywatności i ją szanujemy.</p></b>
+              <div>
+                <label htmlFor="">
+                  <p>Ciasteczka zapamiętujące preferencje użytkownika.</p>
+                  <input type="checkbox" />
+                  <span className="slider"></span>
+                </label>
+                <br></br>
+                <label htmlFor="">
+                  <p>Ciasteczka zbierające anonimowo informacje na temat odwiedzonych części stron.</p>
+                  <input type="checkbox" />
+                  <span className="slider"></span>
+                </label>
+              </div>
+              <div>
+                <button onClick={()=>ustawKrzyżykOkienka(false)}>Potwierdź wszystkie</button>
+                <button onClick={()=>ustawKrzyżykOkienka(false)}>Zapisz Wybór</button>
+              </div>
             </div>
           </div>
         </Popup>
